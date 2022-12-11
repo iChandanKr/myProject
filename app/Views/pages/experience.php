56a5bb8ab916c29a57574a003e5cf654ac111b97
ChandanKumar <chandankumar995584@gmail.com>
2022-12-11 14:23:24 +0530

    crud operations

---
+++ app/Views/pages/experience.php
@@ -0,0 +1,224 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="http://localhost/Crud1/public/Style1.css">
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" integrity="sha512-iBBXm8fW90+nuLcSKlbmrPcLa0OT92xO1BIsZ+ywDWZCvqsWgccV3gFoRBv0z+8dLJgyAHIhR35VZc2oM/gI1w==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link id='favicon' rel="shortcut icon" href="/assets/images/favicon.png" type="image/x-png">
+
+    <title>Experience | Portfolio Chandan</title>
+</head>
+<body oncontextmenu="return false">
+
+<!-- navbar starts -->
+<header>
+        <!-- <a href="/" class="logo"><i class="fab fa-node-js"></i> Chandan</a> -->
+        <a href="<?php echo base_url();?>/index.php/frontPage/" class="logo"> Chandan</a>
+
+        <div id="menu" class="fas fa-bars"></div>
+        <nav class="navbar">
+            <ul>
+            <li><a href="<?php echo base_url();?>/index.php/frontPage/">Home</a></li>
+            <li><a href="/#about">About</a></li>
+            <!-- <li><a href="/#skills">Skills</a></li>
+            <li><a href="/#education">Education</a></li>
+            <li><a href="/#work">Work</a></li> -->
+            <li><a class="active" href="<?php echo base_url();?>/index.php/experience/">Experience</a></li>
+            <!-- <li><a href="/#contact">Contact</a></li> -->
+            </ul>
+        </nav>
+</header>
+<!-- navbar ends -->
+
+<!-- experience section starts -->
+<section class="experience" id="experience">
+
+  <h2 class="heading"><i class="fas fa-briefcase"></i> Experience</h2>
+  <div class="quote">
+      <span>every experience in your life is being orchestrated to teach you something you need to know to move forward.</span>
+  </div>
+
+  <div class="timeline">
+
+    <div class="container left">
+      <div class="content">
+        <div class="tag">
+          <h2>Student</h2>
+        </div>
+        <div class="desc">
+            <h3>Web Application Developer</h3>
+            <p>present</p>
+        </div>
+      </div>
+    </div>
+
+    <div class="container right">
+        <div class="content">
+          <div class="tag">
+            <h2>Programming Languages</h2>
+          </div>
+          <div class="desc">
+              <h3>C/C++ java Python </h3>
+              <p>1st-5th sem</p>
+          </div>
+        </div>
+      </div>
+
+      <div class="container left">
+        <div class="content">
+          <div class="tag">
+            <h2>Version Control System</h2>
+          </div>
+          <div class="desc">
+              <h3>Git/github</h3>
+              <p>5th sem</p>
+          </div>
+        </div>
+      </div>
+
+    <div class="container right">
+      <div class="content">
+        <div class="tag">
+          <h2>Project No 1</h2>
+        </div>
+        <div class="desc">
+            <h3>Bookly</h3>
+            <p>June 2022 - july 2022</p>
+        </div>
+      </div>
+    </div>
+
+    <div class="container left">
+      <div class="content">
+        <div class="tag">
+          <h2>Project No 2</h2>
+        </div>
+        <div class="desc">
+            <h3>CRUD Operation using java </h3>
+            <p>Recent</p>
+        </div>
+      </div>
+    </div>
+
+    <div class="container right">
+      <div class="content">
+        <div class="tag">
+          <h2>Vpro Tech digital</h2>
+        </div>
+        <div class="desc">
+            <h3>Tourism Management  | Traning</h3>
+            <p>July 2022-August 2022</p>
+        </div>
+      </div>
+    </div>
+
+    <div class="container left">
+      <div class="content">
+        <div class="tag">
+          <h2>Problem Solving skills</h2>
+        </div>
+        <div class="desc">
+            <h3>Data structures and Algorithms</h3>
+            <p></p>
+        </div>
+      </div>
+    </div>
+
+    <div class="container right">
+      <div class="content">
+        <div class="tag">
+          <h2>Skill Enhancement Programs</h2>
+        </div>
+        <div class="desc">
+            <h3>AWS</h3>
+            <p></p>
+        </div>
+      </div>
+    </div>
+
+    <div class="container left">
+        <div class="content">
+          <div class="tag">
+            <h2>Public Speaking Skills</h2>
+          </div>
+          <div class="desc">
+              <h3>Presentation on C Languages</h3>
+              <p>19 oct 2022</p>
+          </div>
+        </div>
+      </div>
+
+  </div>
+
+  <div class="morebtn">
+    <a href="<?php echo base_url();?>/index.php/frontPage/" class="btn">
+        <i class="fas fa-arrow-left"></i>
+        <span>Back to Home</span>
+  </a>
+  </div>
+
+</div>
+</section>
+<!-- experience section ends -->
+
+<!-- footer section starts -->
+<section class="footer">
+
+  <div class="box-container">
+
+      <div class="box">
+          <h3>Chandan's Portfolio</h3>
+          <p>Thank you for visiting my personal portfolio website. Connect with me over socials. <br/> </p>
+      </div>
+
+      <div class="box">
+          <h3>quick links</h3>
+          <a href="<?php echo base_url();?>/index.php/frontPage/"><i class="fas fa-chevron-circle-right"></i> home</a>
+          <a href="/#about"><i class="fas fa-chevron-circle-right"></i> about</a>
+          <!-- <a href="/#skills"><i class="fas fa-chevron-circle-right"></i> skills</a>
+          <a href="/#education"><i class="fas fa-chevron-circle-right"></i> education</a>
+          <a href="/#work"><i class="fas fa-chevron-circle-right"></i> work</a> -->
+          <a href="<?php echo base_url();?>/index.php/experience/"><i class="fas fa-chevron-circle-right"></i> experience</a>
+      </div>
+
+      <div class="box">
+          <h3>contact info</h3>
+          <p> <i class="fas fa-phone"></i>+91 73230-41554</p>
+          <p> <i class="fas fa-envelope"></i>chandankumar995584@gmail.com</p>
+          <p> <i class="fas fa-map-marked-alt"></i>Ludhiana, India-141006</p>
+          <div class="share">
+
+            <a href="https://www.linkedin.com/in/chandan-kumar-0ab518212/" class="fab fa-linkedin" target="_blank"></a>
+            <a href="https://github.com/iChandanKr" class="fab fa-github" target="_blank"></a>
+            <!-- <a href="mailto:jigarsable21@gmail.com" class="fas fa-envelope" target="_blank"></a> -->
+            <a href="https://twitter.com/i/flow/login" class="fab fa-twitter" target="_blank"></a>
+            <!-- <a href="https://t.me/lifecode5" class="fab fa-telegram-plane" target="_blank"></a> -->
+          </div>
+      </div>
+  </div>
+
+  <h1 class="credit">Designed with <i class="fa fa-heart pulse"></i> by <a href="https://www.linkedin.com/in/chandan-kumar-0ab518212/">Chandan Kumar</a></h1>
+
+</section>
+<!-- footer section ends -->
+
+<!-- scroll top btn -->
+<a href="#experience" class="fas fa-angle-up" id="scroll-top"></a>
+<!-- scroll back to top -->
+
+
+<!-- ==== ALL MAJOR JAVASCRIPT CDNS STARTS ==== -->
+<!-- jquery cdn -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js" integrity="sha512-894YE6QWD5I59HgZOGReFYm4dnWc1Qt5NtvYSaNcOP+u1T9qYdvdihz0PPSiiqn/+/3e7Jo4EaG7TubfWGUrMQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+
+<!-- scroll reveal anim -->
+<script src="https://unpkg.com/scrollreveal"></script>
+<!-- ==== ALL MAJOR JAVASCRIPT CDNS ENDS ==== -->
+
+<script src="http://localhost/Crud1/public/script.js"></script>
+
+</body>
+</html>
